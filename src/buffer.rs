2c68e20696d703493ccb236a759a3b70b26d1ad1
Babur Makhmudov <bmuddha13@gmail.com>
2024-12-22 00:41:41 +0530

    docs: extend doc comments

--- src/buffer.rs
+++ src/buffer.rs
@@ -9,19 +9,35 @@ use crate::{
     RingAl,
 };
 
-/// todo
+/// Extendable buffer. It dynamically grows to accomodate any extra data beyond the current
+/// capacity, given that there's still some capacity available to allocator itself. Contains a
+/// reference to allocator, thus it effectively locks the allocator, and prevents any further
+/// allocations while any instance of this type exist. After the necessary data is written, the
+/// buffer should be finalized in order to release allocator lock and make the underlying buffer
+/// `Send`
 pub struct ExtBuf<'a> {
+    /// header sequence, containing allocation metadata
     pub(crate) header: Header,
+    /// number of bytes written to buffer so far
     pub(crate) initialized: usize,
+    /// reference to ring allocator instance, used to extend the current buffer
     pub(crate) ringal: &'a mut RingAl,
+    /// finalization flag, used in drop check when buffer is dropped without finalization
     pub(crate) finalized: bool,
 }
 
 impl ExtBuf<'_> {
-    /// todo
+    /// Finalize the write to extendable buffer, releases the allocator lock, and returns another
+    /// buffer type which can be sent to other threads
     pub fn finalize(mut self) -> FixedBufMut {
         let capacity = self.header.capacity();
         let ptr = self.header.buffer();
+        // SAFETY:
+        // 1. the underlying backing store is 'static,
+        // 2. we guarantee that it's a valid address as a consequence of 1
+        // 3. the data is properly aligned
+        // 4. we are constructing slice from the entire available capacity, but FixedBufMut
+        //    guarantees that only initialized section will be exposed
         let inner = unsafe { std::slice::from_raw_parts_mut(ptr, capacity) };
         self.finalized = true;
         FixedBufMut {
@@ -31,28 +47,36 @@ impl ExtBuf<'_> {
         }
     }
 
-    fn copy_from(&mut self, src: &[u8]) {
+    // SAFETY: obviously this is outrageously unsafe, but the callers of this method always
+    // guarantee the invariants, namely:
+    // 1. underlying buffer never overlaps with src,
+    // 2. the length of src never exceeds remaining capacity of buffer
+    unsafe fn copy_from(&mut self, src: &[u8]) {
         let count = src.len();
         let src = src.as_ptr();
-        unsafe {
-            let buffer = self.header.buffer().add(self.initialized);
-            buffer.copy_from_nonoverlapping(src, count)
-        };
+        let buffer = self.header.buffer().add(self.initialized);
+        buffer.copy_from_nonoverlapping(src, count);
         self.initialized += count;
     }
 }
 
 impl Write for ExtBuf<'_> {
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        // optimization for edge case recursion
         if buf.is_empty() {
             return Ok(0);
         }
+        // make sure that we have enough spare capacity to fit the buf.len() bytes
         let available = self.header.capacity() - self.initialized;
         let count = buf.len();
         if available >= count {
-            self.copy_from(buf);
+            // SAFETY: safe, we exclusively own the memory region with ExtBuf, so
+            // the buf cannot overlap with it, we made sure that the underlying
+            // buffer still have enough capacity to accomodate the length of buf
+            unsafe { self.copy_from(buf) };
             return Ok(count);
         }
+        // extend the buffer with the missing capacity
         let required = buf.len() - available;
         let Some(header) = self.ringal.alloc(required) else {
             return Err(io::Error::new(
@@ -60,18 +84,38 @@ impl Write for ExtBuf<'_> {
                 "allocator's capacity exhausted",
             ));
         };
+        // we've just wrapped around the ring, but we need continuous
+        // memory, so we are forced to perform copy of bytes from tail
+        // to head, which might potentially fail due to lack of capacity
         if header < self.header {
+            // get pointer to current buffer
             let ptr = self.header.buffer();
+            // SAFETY:
+            // 1. the underlying backing store is 'static,
+            // 2. we guarantee that it's a valid address as a consequence of 1
+            // 3. the data is properly aligned
+            // 4. we are only constructing slice from initialized data
             let old = unsafe { std::slice::from_raw_parts(ptr, self.initialized) };
+            // keep the guard around to auto-release the memory
+            // in tail, irrespective of what happens next
             let _guard = Guard::from(self.header);
+            // wrap around adjustment of pointer
             self.header = header;
+            // lock the new memory region
             self.header.set();
+            // copy existing data from tail to new memory region
             let _ = self.write(old)?;
+            // copy argument buffer
             return self.write(buf);
         }
+        // readjust the next guard pointer in case if fragmentation happened
         let end = header.next().inner();
         self.header.store(end);
-        self.copy_from(buf);
+        // SAFETY: safe, we exclusively own the memory region
+        // with `ExtBuf`, so the buf cannot overlap with it, we
+        // extended the memory region with extra capacity so that
+        // it now has enough to accomodate the length of `buf`
+        unsafe { self.copy_from(buf) };
         Ok(count)
     }
 
@@ -80,6 +124,8 @@ impl Write for ExtBuf<'_> {
     }
 }
 
+// Custom Drop implementation in order to handle edge
+// case, when ExtBuf is dropped without being finalized
 impl Drop for ExtBuf<'_> {
     fn drop(&mut self) {
         if !self.finalized {
@@ -89,16 +135,20 @@ impl Drop for ExtBuf<'_> {
     }
 }
 
-///f afsf
+/// Fixed length (not growable) mutable buffer
 pub struct FixedBufMut {
+    /// the lock release mechanism upon drop
     pub(crate) _guard: Guard,
+    /// exclusively owned slice from backing store
     pub(crate) inner: &'static mut [u8],
+    /// number of initialized bytes
     pub(crate) initialized: usize,
 }
 
-/// todo
 impl FixedBufMut {
-    /// todo
+    /// make the buffer immutable and cheaply cloneable,
+    /// this method involves heap allocation, don't use
+    /// it unless you need to clone the buffer
     pub fn freeze(self) -> FixedBuf {
         FixedBuf {
             _rc: Arc::new(self._guard),
@@ -106,7 +156,7 @@ impl FixedBufMut {
         }
     }
 
-    /// todo
+    /// available (uninitialized) capacity in bytes
     pub fn spare(&self) -> usize {
         self.inner.len() - self.initialized
     }
@@ -143,7 +193,7 @@ impl Write for FixedBufMut {
     }
 }
 
-/// todo
+/// Immutable and cheaply cloneable buffer
 #[derive(Clone)]
 pub struct FixedBuf {
     _rc: Arc<Guard>,
--- src/header.rs
+++ src/header.rs
@@ -3,59 +3,78 @@ use std::sync::atomic::{AtomicUsize, Ordering::*};
 use crate::USIZELEN;
 
 #[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
+/// Helper type that wraps memory guard sequence
 pub(crate) struct Header(*mut AtomicUsize);
+/// Allocation guard, used through its Drop implementation, which
+/// unlocks (releases) the memory region back to allocator
 pub(crate) struct Guard(&'static AtomicUsize);
 
 impl Header {
+    /// lock the memory region, preventing allocator
+    /// from reusing it until it's unlocked
     pub(crate) fn set(&self) {
         unsafe { &*self.0 }.fetch_or(1, Release);
     }
 
+    /// construct new Header from pointer data
+    /// (next guard) stored in some guard sequence
     pub(crate) fn load(storage: usize) -> Self {
         let ptr = (storage >> 1) as *mut usize;
         Self::new(ptr)
     }
 
+    /// store next guard pointer data in current header
     pub(crate) fn store(&self, next: *mut usize) {
         let storage = (next as usize) << 1;
         unsafe { &*self.0 }.store(storage, Release);
     }
 
+    /// construct new header from guard sequence pointer, this
+    /// allows to perform various atomic operation on guard sequence
     pub(crate) fn new(ptr: *mut usize) -> Self {
         Self(ptr as *mut AtomicUsize)
     }
 
+    /// jump to next guard sequence and construct new Header from it
     pub(crate) fn next(&self) -> Self {
         let storage = unsafe { &*self.0 }.load(Acquire);
         Self::load(storage)
     }
 
+    /// checks whether or not guarded memory region is locked
     pub(crate) fn available(&self) -> bool {
         unsafe { &*self.0 }.load(Acquire) & 1 == 0
     }
 
+    /// distance (in `size_of<usize>()`) to given guard
     pub(crate) fn distance(&self, end: &Self) -> usize {
-        if self >= end {
-            println!("NEGATIVE distance!!!!!!!!!!!!!!");
-        }
+        // SAFETY: its just a pointer arithmetic really, the caller
+        // guarantees that both pointers point into the backing store
+        // of allocator and `end` is always greater than `self`
         (unsafe { end.0.offset_from(self.0) }) as usize - 1
     }
 
+    /// get the raw pointer to guard sequence
     pub(crate) fn inner(self) -> *mut usize {
         self.0 as *mut usize
     }
 
+    /// calculate the total available capacity
+    /// (in bytes) of guarded memory region
     pub(crate) fn capacity(&self) -> usize {
         let end = self.next();
         self.distance(&end) * USIZELEN
     }
 
+    /// get the raw pointer to guarded memory region
     pub(crate) fn buffer(&self) -> *mut u8 {
         (unsafe { self.0.add(1) }) as *mut u8
     }
 }
 
 impl Guard {
+    /// release the lock from memory region,
+    /// making it available to allocator
     fn unset(&self) {
         self.0.fetch_and(usize::MAX << 1, Release);
     }
